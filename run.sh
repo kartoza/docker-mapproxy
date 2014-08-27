2bc04d84d11f12ae66a4d71f01cb4ff06168aedb
Tim Sutton <tim@linfiniti.com>
2014-08-27 12:55:01 +0200

    Fixed run script

--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@ docker kill mapproxy
 docker rm mapproxy
 mkdir mapproxy
 chmod a+wX mapproxy
-docker run --name="mapproxy" -v `pwd`/mapproxy:/mapproxy -p 1234:80 -i -t kartoza/mapproxy /bin/bash
+docker run --name="mapproxy" -v `pwd`/mapproxy:/mapproxy -p 1234:80 -d -t kartoza/mapproxy
 docker logs mapproxy
