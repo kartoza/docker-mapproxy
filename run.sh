55c30dd5a1f7e28f459028363e401886baf95c68
admire <admire@kartoza.com>
2020-11-15 15:03:05 +0200

    add some config options for mapproxy

--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-docker kill mapproxy
-docker rm mapproxy
-mkdir mapproxy
-chmod a+wX mapproxy
-#docker run --name="mapproxy" -v `pwd`/mapproxy:/mapproxy -p 1234:8080 -d -t kartoza/mapproxy
-docker run --name="mapproxy" -v `pwd`/mapproxy:/mapproxy -p 1234:8080 -i -t kartoza/mapproxy /bin/bash
-docker logs mapproxy
