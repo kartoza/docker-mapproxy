92b2d453b4206bdf086ae5e6d88be77b19973968
Shlomi k <65117898+CL-SHLOMIKONCHA@users.noreply.github.com>
2021-11-24 11:39:35 +0200

    chore: added logging module (#9)

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 # WSGI module for use with Apache mod_wsgi or gunicorn
 
 # # uncomment the following lines for logging
-# # create a log.ini with `mapproxy-util create -t log-ini`
-# from logging.config import fileConfig
-# import os.path
-# fileConfig(r'/mapproxy/log.ini', {'here': os.path.dirname(__file__, reloader=True)}, reloader=True)
+# create a log.ini with `mapproxy-util create -t log-ini`
+from logging.config import fileConfig
+import os.path
+fileConfig(r'/mapproxy/log.ini', {'here': os.path.dirname(__file__)})
 
 import os
 from mapproxy.wsgiapp import make_wsgi_app
