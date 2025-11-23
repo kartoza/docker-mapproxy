6b2a7e5f278863325f24df4cd4d7e5ef33869b5b
Admire Nyakudya <mazano@za-nyakudya-mac.local>
2025-11-23 12:49:27 +0200

    upgrade version to latest

--- build_data/multi_mapproxy.py
+++ build_data/multi_mapproxy.py
@@ -1,3 +1,3 @@
 from mapproxy.multiapp import make_wsgi_app
-application = make_wsgi_app('${MULTI_MAPPROXY_DATA_DIR}', allow_listing=${ALLOW_LISTING})
+application = make_wsgi_app('${MULTI_MAPPROXY_DATA_DIR}', allow_listing="${ALLOW_LISTING}")
 
